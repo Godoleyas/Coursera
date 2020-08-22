c225d9d9bd5f50e197bfc33d4ed48b50c62de5eb
Godoleyas <papygamer321@gmail.com>
2020-08-22 06:30:16 -0700

    some updates

---
+++ google/script.js
@@ -0,0 +1,5 @@
+var voornaam = document.getElementById("Leokun@gmail.com").value;
+        var achternaam = document.getElementById("Leokun").value;
+        var mail = document.getElementById("Leokun@gmail.com").value;
+        var password = document.getElementById("easypeasy").value;
+        var gender = document.getElementById("Male").value;
