cf0651385e7ec0159ca9147a83a6f910ef9f6d43
Godoleyas <papygamer321@gmail.com>
2020-08-23 07:31:08 -0700

    Some coursera updates

--- google/script.js
+++
@@ -1,5 +0,0 @@
-var voornaam = document.getElementById("Leokun@gmail.com").value;
-        var achternaam = document.getElementById("Leokun").value;
-        var mail = document.getElementById("Leokun@gmail.com").value;
-        var password = document.getElementById("easypeasy").value;
-        var gender = document.getElementById("Male").value;
